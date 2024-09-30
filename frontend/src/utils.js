300d5a6c774da9c6602eacaa0e3ced9207d5a928
Boyang Hu <boyanghu98@gmail.com>
2024-10-01 04:35:18 +1000

    Update frontend deploy setting

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,8 +1,11 @@
 // If you don't want to host your server code and client code together, you can 
 // pay AWS to host your server with HTTPS then config the api url endpoints like below
 // const SERVER_ORIGIN = '<Your server's url>'; 
-const SERVER_ORIGIN = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+// const SERVER_ORIGIN = 'http://host.docker.internal:8080' || 'http://localhost:8080';
+const SERVER_ORIGIN =  'http://localhost:8080';
 // const SERVER_ORIGIN = '';
+
+
 const loginUrl = `${SERVER_ORIGIN}/login`;
 
 export const login = (credential) => {
@@ -12,24 +15,26 @@ export const login = (credential) => {
       'Content-Type': 'application/json',
     },
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
     body: JSON.stringify(credential)
   }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to log in');
     }
- 
+
     return response.json();
   })
 }
- 
+
 const registerUrl = `${SERVER_ORIGIN}/register`;
- 
+
 export const register = (data) => {
   return fetch(registerUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    mode: 'cors',  // Ensure you're explicitly setting this
     body: JSON.stringify(data)
   }).then((response) => {
     if (response.status !== 200) {
@@ -37,67 +42,75 @@ export const register = (data) => {
     }
   })
 }
- 
+
 const logoutUrl = `${SERVER_ORIGIN}/logout`;
- 
+
 export const logout = () => {
   return fetch(logoutUrl, {
     method: 'POST',
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
+
   }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to log out');
     }
   })
 }
- 
+
 const topGamesUrl = `${SERVER_ORIGIN}/game`;
- 
+
 export const getTopGames = () => {
-  return fetch(topGamesUrl).then((response) => {
+  return fetch(topGamesUrl,{
+    mode: 'cors',  // Ensure you're explicitly setting this
+  }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to get top games');
     }
- 
+
     return response.json();
   })
 }
- 
+
 const getGameDetailsUrl = `${SERVER_ORIGIN}/game?game_name=`;
- 
+
 const getGameDetails = (gameName) => {
-  return fetch(`${getGameDetailsUrl}${gameName}`).then((response) => {
+  return fetch(`${getGameDetailsUrl}${gameName}`,{
+    mode: 'cors',  // Ensure you're explicitly setting this
+  }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to find the game');
     }
- 
+
     return response.json();
   });
 }
- 
+
 const searchGameByIdUrl = `${SERVER_ORIGIN}/search?game_id=`;
- 
+
 export const searchGameById = (gameId) => {
-  return fetch(`${searchGameByIdUrl}${gameId}`).then((response) => {
+  return fetch(`${searchGameByIdUrl}${gameId}`, {
+    mode: 'cors',  // Ensure you're explicitly setting this
+  }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to find the game');
     }
     return response.json();
   })
 }
- 
+
 export const searchGameByName = (gameName) => {
   return getGameDetails(gameName).then((data) => {
     if (data && data.id) {
       return searchGameById(data.id);
     }
- 
+
     throw Error('Fail to find the game')
   })
 }
- 
+
 const favoriteItemUrl = `${SERVER_ORIGIN}/favorite`;
- 
+
 export const addFavoriteItem = (favItem) => {
   return fetch(favoriteItemUrl, {
     method: 'POST',
@@ -105,6 +118,7 @@ export const addFavoriteItem = (favItem) => {
       'Content-Type': 'application/json',
     },
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
     body: JSON.stringify({ favorite: favItem })
   }).then((response) => {
     if (response.status !== 200) {
@@ -112,7 +126,7 @@ export const addFavoriteItem = (favItem) => {
     }
   })
 }
- 
+
 export const deleteFavoriteItem = (favItem) => {
   return fetch(favoriteItemUrl, {
     method: 'DELETE',
@@ -120,6 +134,7 @@ export const deleteFavoriteItem = (favItem) => {
       'Content-Type': 'application/json',
     },
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
     body: JSON.stringify({ favorite: favItem })
   }).then((response) => {
     if (response.status !== 200) {
@@ -127,29 +142,31 @@ export const deleteFavoriteItem = (favItem) => {
     }
   })
 }
- 
+
 export const getFavoriteItem = () => {
   return fetch(favoriteItemUrl, {
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
   }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to get favorite item');
     }
- 
+
     return response.json();
   })
 }
- 
+
 const getRecommendedItemsUrl = `${SERVER_ORIGIN}/recommendation`;
- 
+
 export const getRecommendations = () => {
   return fetch(getRecommendedItemsUrl, {
     credentials: 'include',
+    mode: 'cors',  // Ensure you're explicitly setting this
   }).then((response) => {
     if (response.status !== 200) {
       throw Error('Fail to get recommended item');
     }
- 
+
     return response.json();
   })
 }
