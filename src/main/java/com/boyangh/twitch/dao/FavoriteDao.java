2f3551184d5b5c385a3e996561a7f1b2efdf0df7
Boyang Hu <boyanghu98@gmail.com>
2024-09-30 12:44:37 +1000

    Implement Recommendation components

--- src/main/java/com/boyangh/twitch/dao/FavoriteDao.java
+++ src/main/java/com/boyangh/twitch/dao/FavoriteDao.java
@@ -64,4 +64,40 @@ public class FavoriteDao {
         }
         return new HashSet<>();
     }
+
+    // Get favorite item ids for the given user
+    public Set<String> getFavoriteItemIds(String userId) {
+        Set<String> itemIds = new HashSet<>();
+
+        try (Session session = sessionFactory.openSession()) {
+            Set<Item> items = session.get(User.class, userId).getItemSet();
+            for (Item item : items) {
+                itemIds.add(item.getId());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return itemIds;
+    }
+
+    // Get favorite items for the given user. The returned map includes
+    // three entries like {"Video": [item1, item2, item3], "Stream": [item4, item5, item6], "Clip": [item7, item8, ...]}
+    public Map<String, List<String>> getFavoriteGameIds(Set<String> favoriteItemIds) {
+        Map<String, List<String>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), new ArrayList<>());
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            for (String itemId : favoriteItemIds) {
+                Item item = session.get(Item.class, itemId);
+                itemMap.get(item.getType().toString()).add(item.getGameId());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return itemMap;
+    }
+
+
 }
